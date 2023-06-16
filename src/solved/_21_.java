26fcee78018c7598a4f3216a46a80e53ef1f2ba6
davidpokol <90441129+davidpokol@users.noreply.github.com>
2023-06-16 18:46:20 +0200

    Update _21_.java

--- src/solved/_21_.java
+++ src/solved/_21_.java
@@ -5,38 +5,29 @@ public class _21_ {
     public static void main (String[] args) {
 
         System.out.println(isHaveDivider(new int[] {2,23423243}));
-
     }
 
     private static boolean isHaveDivider(int[] array) {
 
-        for(int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
 
-            int j;
-            for(j = 0; j < array.length; j++) {
-
-                if(i == j) {
+                if (i == j) {
                     continue;
                 }
-
                 int a;
                 int b;
-
-                if(array[i] > array[j]) {
+                if (array[i] > array[j]) {
                     a = array[i];
                     b = array[j];
-
                 } else {
                     a = array[j];
                     b = array[i];
-
                 }
-
-                if(a % b !=0) {
+                if (a % b != 0) {
                     break;
                 }
-
-                if(j == array.length - 1) {
+                if (j == array.length - 1) {
                     return true;
                 }
             }
