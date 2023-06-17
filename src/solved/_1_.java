aab6876eabe5db1483f2ac27310e426853ad3d4c
davidpokol <90441129+davidpokol@users.noreply.github.com>
2023-06-17 15:19:00 +0200

    Update _1_.java

--- src/solved/_1_.java
+++ src/solved/_1_.java
@@ -19,7 +19,7 @@ public class _1_ {
             return false;
         }
 
-        for(int i = 2; i< number/2; i++) {
+        for(int i = 2; i <=number/2; i++) {
 
             if(number % i == 0) {
                 return false;
