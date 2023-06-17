bbd5fc3ad1527878ea8d9dc503d58c82249df168
davidpokol <90441129+davidpokol@users.noreply.github.com>
2023-06-17 17:38:09 +0200

    Update _7_.java

--- src/solved_by_chatgpt/_7_.java
+++ src/solved_by_chatgpt/_7_.java
@@ -3,27 +3,22 @@ package solved_by_chatgpt;
 public class _7_ {
 
     public static void main(String[] args) {
-        System.out.println(getIntegerPower(0));
-
-        System.out.println(Math.pow(15,2));
-
+        System.out.println(Math.pow(15,3));
+        System.out.println(getIntegerPower(15));
     }
 
 
     private static int getIntegerPower(int num) {
 
-        int i = 0;
         int power = 1;
 
-        if(num < 2) {
+        if (num < 2) {
             return -1;
         }
 
-        while (power * num <= 567) {
+        while (power * num < 567) {
             power *= num;
-            i++;
         }
-
-        return i;
+        return power;
     }
 }
